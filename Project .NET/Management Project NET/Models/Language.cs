a8b3d43e824e0c3736cddfdad3cff96866235f8f
ElstJ-AP <jonas.elst@hotmail.com>
2016-11-14 14:57:50 +0100

    Init

---
+++ Project .NET/Management Project NET/Models/Language.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
+    }
+}
