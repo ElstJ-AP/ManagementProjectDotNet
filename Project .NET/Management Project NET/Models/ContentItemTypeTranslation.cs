5ffa9beb43c3a654a51dec2cd3b9a800de7df4e0
ElstJ-AP <jonas.elst@hotmail.com>
2016-11-14 15:24:49 +0100

    Added translation classes

---
+++ Project .NET/Management Project NET/Models/ContentItemTypeTranslation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ContentItemTypeTranslation
+    {
+        public int ContentItemTypeId { get; set; }
+        public ContentItemType ContentItemType { get; set; }
+        public int LanguageId { get; set; }
+        public Language Language { get; set; }
+        [Required]
+        [Column(TypeName = "text")]
+        public string Translation { get; set; }
+        
+    }
+}
---
+++ Project .NET/Management Project NET/Models/RoleTranslation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class RoleTranslation
+    {
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public int LanguageId { get; set; }
+        public Language Language { get; set; }
+        [Required]
+        [Column(TypeName = "text")]
+        public string Translation { get; set; }
+        
+    }
+}
