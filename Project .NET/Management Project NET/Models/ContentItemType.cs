5d876326dd5dec1c5538bbfb708031ce5be833df
JaspervdK <vandekantjasper@gmail.com>
2016-11-14 15:36:06 +0100

    JVDK: commit test visualstudio15

---
+++ Project .NET/Management Project NET/Models/ContentItemType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ContentItemType
+    {
+        public int Id { get; set; }
+        [Index(IsUnique =true), MaxLength(255), Required]
+        public string Name { get; set; }
+    }
+}
